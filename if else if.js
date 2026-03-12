76457023a23928c5ae5327ee6839775b9cc1a092
Navyateja330 <navyatejae3@gmail.com>
2026-03-12 12:17:56 +0530

    Add files via upload

---
+++ if else if.js
@@ -0,0 +1,9 @@
+let aadhar = false
+let pan = true;
+if (aadhar){
+    console.log('you can open account in your bank');
+}else if (pan){
+    console.log('you can open account in your bank');
+}else{
+    console.log('you cannot open account in your bank');
+}
---
+++ if else.js
@@ -0,0 +1,9 @@
+let age = 6;
+let d1 = false
+if(age > 18 ||d1 == true){
+    console.log("you can drive");
+
+}
+else{
+    console.log("you cannot drive");
+}
